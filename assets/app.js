070af9b60bc2626a47f7f74ef296df9049a3cc51
shadi38485 <shadisalehi38485@gmail.com>
2024-02-24 13:18:12 +0100

    learn array map and set indicators

--- assets/app.js
+++ assets/app.js
@@ -1,26 +1,47 @@
-const images = document.querySelectorAll(".image-box img");
-console.log(images);
+const images = document.querySelectorAll(".images-box img");
+const indicators = document.querySelectorAll(".indicators span");
+// console.log(images);
+// console.log(indicators);
 
 let index = 0;
 
 document.querySelector(".next").addEventListener("click", () => {
   images[index].style.display = "none";
+  indicators[index].style.opacity = "40%";
   index++;
   if (index == images.length) {
     index = 0;
   }
   images[index].style.display = "block";
+  indicators[index].style.opacity = "100%";
 });
 
 document.querySelector(".previous").addEventListener("click", function () {
   images[index].style.display = "none";
+  indicators[index].style.opacity = "40%";
   index--;
   if (index == -1) {
     index = images.length - 1;
   }
   images[index].style.display = "block";
+  indicators[index].style.opacity = "100%";
 });
 
+indicators.forEach((item, i) => {
+  
+  item.addEventListener("click", () => {
+    images.forEach((item) => {
+      item.style.display = "none";
+    });
+
+    indicators.forEach((item) => {
+      item.style.opacity = "40%";
+    });
+
+    images[i].style.display = "block";
+    indicators[i].style.opacity = "100%";
+  });
+});
 
 /// time
 
